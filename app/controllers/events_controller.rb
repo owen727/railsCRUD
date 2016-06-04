d4213ec5fc674e65fffc0b1e7bbd525f092d5d06
Owen Ou <matsu727@me.com>
2016-06-04 12:47:53 +0800

    New and Show added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,18 +5,18 @@ def index
     
     @events = Event.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml {
-        render :xml => @events.to_xml 
-      }
-      format.json {
-        render :json => @events.to_json
-      }
-      format.atom {
-        @feed_title = "My event list"
-      } #index.atom.builder
-    end
+    # respond_to do |format|
+    #   format.html # index.html.erb
+    #   format.xml {
+    #     render :xml => @events.to_xml 
+    #   }
+    #   format.json {
+    #     render :json => @events.to_json
+    #   }
+    #   format.atom {
+    #     @feed_title = "My event list"
+    #   } #index.atom.builder
+    # end
   end
 
   # GET /events/new  #show form
@@ -24,6 +24,18 @@ def index
     @event = Event.new
   end
 
+  def create
+    @event = Event.new( event_params )
+    
+    @event.save
+    
+    redirect_to events_path # HTTP code: 303
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    
+  end
 
 
 
@@ -31,15 +43,19 @@ def index
 
 
 
+  def destroy
+    @event = Event.find(params[:id])
+    @event.delete
+    redirect_to events_path
 
 
+  end  
 
 
-
-
-
-
-
-
+  private
+    
+  def event_params
+      params.require(:event).permit(:name, :description, :isbn)
+  end
 
 end
