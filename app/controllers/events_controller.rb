22cfa72a1f1c71f31f86cfc4d93f7860eb25ce7a
Owen Ou <matsu727@me.com>
2016-06-05 01:29:01 +0800

    CRUD flash validation kaminari

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,10 @@
 class EventsController < ApplicationController
 
+  before_action :set_event, :only => [:show, :edit, :update, :destroy]
 
-def index
+  def index
     
-    @events = Event.all
+    @events = Event.page( params[:page] ).per(10)
 
     # respond_to do |format|
     #   format.html # index.html.erb
@@ -17,6 +18,9 @@ def index
     #     @feed_title = "My event list"
     #   } #index.atom.builder
     # end
+
+    # @page_title = @events.name 比較喜歡固定title
+
   end
 
   # GET /events/new  #show form
@@ -26,41 +30,50 @@ def index
 
   def create
     @event = Event.new( event_params )
-    
-    @event.save
-    
-    redirect_to events_path # HTTP code: 303
+    if @event.save #save成功會回傳true
+      flash[:notice] = "New Book Added"
+      redirect_to events_path # HTTP code: 303
+    else
+      render :action => :new
+    end
+
   end
 
   def show
-    @event = Event.find(params[:id])
-    
+    @page_title = @event.name
   end
 
   def edit
-    @event = Event.find(params[:id])
+    @page_title = @event.name
   end
 
-  def update
-    @event = Event.find(params[:id])
-    @event.update(event_params)
-    redirect_to event_path(@event)
+  def update  
+   if @event.update(event_params)
+      flash[:notice] = "Information Updated "
+      redirect_to event_path(@event)
+    else
+      render :action => :edit
+    end 
     
   end
 
   def destroy
-    @event = Event.find(params[:id])
     @event.delete
+    flash[:alert] = "Book Deleted"
     redirect_to events_path
-
-
   end  
+
+
 
 
   private
     
   def event_params
       params.require(:event).permit(:name, :description, :isbn)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 
 end
